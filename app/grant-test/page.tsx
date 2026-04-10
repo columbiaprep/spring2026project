f5793ff061aaa8112922f35b2d1f6ebda9f2e2ce
Grant Chereskin <gchereskin27@cgps.org>
2026-04-10 11:49:44 -0400

    added first floor rooms

--- app/grant-test/page.tsx
+++ app/grant-test/page.tsx
@@ -10,6 +10,17 @@ import { useGLTF, OrbitControls } from '@react-three/drei'
 import { MusicRoom1Model } from './MusicRoom1Model'
 import { MusicHallwayModel } from './MusicHallwayModel'
 import { MusicRoom2Model } from './MusicRoom2Model'
+import { LoungeModel } from './LoungeModel'
+import { FrontDeskModel } from './FrontDeskModel'
+import { LobbyHallway } from './LobbyHallway'
+import { Model113N } from './113N'
+import { Model110N } from './110N'
+import { Model109N } from './109N'
+import { CorridorModel } from './CorridorModel'
+import { TechHubModel } from './TechHubModel'
+import { OutsideTechHubModel } from './OutsideTechHubModel'
+import { West92streetDeskModel } from './West92streetDeskModel'
+import { UpperLibraryModel } from './UpperLibraryModel'
 
 export default function Grant() {
 
@@ -17,25 +28,54 @@ export default function Grant() {
 
     return (
         <div style={{ width: '100vw', height: '100vh' }}>
-            <Canvas>
+            <Canvas camera={{ position: [-2, 15, 10], fov: 50 }}>
                 <Suspense fallback={null}>
-                        <ambientLight intensity={Math.PI / 2} />
+                    <ambientLight intensity={Math.PI / 2} />
 
-                        <MusicHallwayModel position={[0, 0, 0]} />
+                    <FrontDeskModel position={[7, 0.2, -9.15]} rotation={[0, Math.PI, 0]} />
+                    <LoungeModel position={[5.3, 0.35, 15.3]} />
+                    <LobbyHallway position={[8.14, 0.1, 4.53]} />
+                    <OutsideTechHubModel position={[2.1, 0.27, 23.99]} />
+                    <West92streetDeskModel position={[5.15, 1, 31.96]} rotation={[0, Math.PI/2, 0]} />
 
-                        <Room 
-                        model={MusicRoom1Model}
-                        position={[0.8, -0.57, 2.1]}
-                        rotation={[0, Math.PI, 0]}
-                        />
+                    <MusicHallwayModel position={[0, 0, 0]} />
 
-                        <Room 
-                        model={MusicRoom2Model}
-                        position={[-5.93, 0.17, -5.2]}
-                        rotation={[0, Math.PI, 0]}
-                        />
-                        
-                        <OrbitControls />
+                    <CorridorModel position={[-13.78, 0.17, 11.64]} rotation={[0, Math.PI/2, 0]} />
+                    
+                    <UpperLibraryModel position={[3.52, 0.25, -12.73]} rotation={[0, Math.PI, 0]} />
+
+                    <Room
+                    model={TechHubModel}
+                    position={[9.3, 0.83, 25.65]}
+                    rotation={[0, Math.PI, 0]}
+                    />
+                    <Room 
+                    model={MusicRoom1Model}
+                    position={[0.8, -0.57, 2.1]}
+                    rotation={[0, Math.PI, 0]}
+                    />
+                    <Room 
+                    model={MusicRoom2Model}
+                    position={[-5.93, 0.17, -5.2]}
+                    rotation={[0, Math.PI, 0]}
+                    />
+                    <Room
+                    model={Model113N}
+                    position={[-5.93, 0.09, 5.5]}
+                    rotation={[0, Math.PI, 0]}
+                    />
+                    <Room
+                    model={Model110N}
+                    position={[-3.55, 0.35, -12.38]}
+                    rotation={[0, 2 * Math.PI, 0]}
+                    />
+                    <Room
+                    model={Model109N}
+                    position={[0.57, 0.07, -12.27]}
+                    rotation={[0, 2 * Math.PI, 0]}
+                    />
+                    
+                    <OrbitControls />
                 </Suspense>
             </Canvas>
         </div>
@@ -53,6 +93,7 @@ function Room({ model: Model, ...props }) {
       color = {hovered ? [0.5, 0.6, 1] : 'white'}
       hovered={hovered}
       active={active}
+      
       onPointerEnter={(e) => {
         e.stopPropagation()
         setHovered(true)
