c4f2dad8af32dbb31b1065c3bbd70565c708e8d8
Jake Weiner <jakeweiner@Jakes-MacBook-Air-2.local>
2026-04-24 12:24:17 -0400

    firebase

--- firebase-config.ts
+++ firebase-config.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
-import { get } from "http";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
